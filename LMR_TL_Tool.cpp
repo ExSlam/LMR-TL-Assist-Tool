7932d7afef73a54c9dd010d1f90e1cf4b2745c09
ExSlam <114887800+ExSlam@users.noreply.github.com>
2023-09-15 15:53:23 -0400

    Cleaned up Main function for better readability
    Split the code into 3 functions that are called in the main() function for better readability. No functional changes for end users.

--- LMR_TL_Tool.cpp
+++ LMR_TL_Tool.cpp
@@ -1,57 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <chrono>
-#include <thread>
 #include <codecvt>
 #include <cstdlib>
 #include <regex>
 
-int main() {
-    //this program tags dialogue with text tags for translation support and creates a translation csv file and populates it with tags and empty strings, where translations need to be
-    //written manually.
 
-    //this section is needed to set the locale to utf-8 so that characters from languages like cryllic are encoded properly
-    const std::locale empty_locale = std::locale::empty();
-    typedef std::codecvt_utf8<wchar_t> converter_type;
-    const converter_type* converter = new converter_type;
-    const std::locale utf8_locale = std::locale(empty_locale, converter);
+//I'm not sure if wide intput and wide output fstreams and wide strings are necessary to keep the utf-8 encoding, but the .imbue(utf8_locale) line is necessary for reading/writing in cryllic.
+void openFileInUTF8(std::wifstream& wideInputFilestream, const std::locale& locale)
+{
+    wideInputFilestream.imbue(locale);
 
-    std::string sourceFileName, translationFileName, updatedSourceFileName;
-    std::cout << "Enter the location of the source file (name of story script text file followed by .txt): ";
-    std::cin >> sourceFileName;
-    std::cout << "Enter the location of the translation (.csv) file to create: ";
-    std::cin >> translationFileName;
-    std::cout << "Enter the location of the updated source file with tags (.txt) to create: ";
-    std::cin >> updatedSourceFileName;
-    
-    //I'm not sure if wide intput and wide output fstreams and wide strings are necessary to keep the utf-8 encoding, but the .imbue(utf8_locale) line is necessary.
-    std::wifstream sourceFile(sourceFileName);
-    //Turn the text buffer stream into a utf-8 encoded string.
-    sourceFile.imbue(utf8_locale);
-    if (!sourceFile.is_open()) 
+    if (!wideInputFilestream.is_open())
     {
-        std::cerr << "Error: Source file not found or could not be opened." << std::endl;
-        /*FILE* outFile = fopen(sourceFileName, "w+,ccs=UTF-8");*/
-        return 1;
+        std::cerr << "Error: File could not be found or could not be opened.\n";
     }
+}
 
-    std::wofstream translationFile(translationFileName);
-    translationFile.imbue(utf8_locale);
-    if (!translationFile.is_open()) 
+//wofstream overload for output streams
+void openFileInUTF8(std::wofstream& wideInputFilestream, const std::locale& locale)
+{
+    wideInputFilestream.imbue(locale);
+
+    if (!wideInputFilestream.is_open())
     {
-        std::cerr << "Error: translation file could not be created or could not be opened." << std::endl;
-        return 1;
+        std::cerr << "Error: File could not be found or could not be opened.\n";
     }
+}
 
-    std::wofstream updatedSourceFile(updatedSourceFileName);
-    updatedSourceFile.imbue(utf8_locale);
-    if (!updatedSourceFile.is_open()) 
-    {
-        std::cerr << "Error: updated source file could not created or could not be opened." << std::endl;
-        return 1;
-    }
-
+//Create and format a csv file for manual translation insertion, and to create a script file formatted to support translations from a csv file
+void formatTextFilesForTranslation(std::wifstream& sourceFile, std::wofstream& translationFile, std::wofstream& updatedSourceFile)
+{
     int textCount = 1;
     std::wstring sourceLine;
     const std::wstring CSV_QUOTATIONS = L"\"\"\"";
@@ -71,7 +50,7 @@ int main() {
             //check whether there are tabs or single spaces before the start of a line with dialogue and insert the tag after the tab or spaces
             std::size_t updatedLineStartPos = sourceLine.find_first_not_of(L' ');
             std::size_t firstTabPos = sourceLine.find_first_not_of(L'\t');
-            if (firstTabPos > updatedLineStartPos) 
+            if (firstTabPos > updatedLineStartPos)
             {
                 updatedLineStartPos = firstTabPos;
             }
@@ -96,6 +75,40 @@ int main() {
     sourceFile.close();
     translationFile.close();
     updatedSourceFile.close();
+}
+
+
+int main() {
+    //this program tags dialogue with text tags for translation support and creates a translation csv file and populates it with tags and empty strings, where translations need to be
+    //written manually.
+
+    //this section is needed to set the locale to utf-8 so that characters from languages like cryllic are encoded properly
+    const std::locale empty_locale = std::locale::empty();
+    typedef std::codecvt_utf8<wchar_t> converter_type;
+    const converter_type* converter = new converter_type;
+    const std::locale utf8_locale = std::locale(empty_locale, converter);
+
+    //assumes file names are in english alphabet
+    std::string sourceFileName, translationFileName, updatedSourceFileName;
+
+    std::cout << "Enter the location of the source file (name of story script text file followed by .txt): ";
+    std::cin >> sourceFileName;
+    std::cout << "Enter the location of the translation (.csv) file to create: ";
+    std::cin >> translationFileName;
+    std::cout << "Enter the location of the updated source file with tags (.txt) to create: ";
+    std::cin >> updatedSourceFileName;
+    
+    
+    std::wifstream sourceFile(sourceFileName);
+    std::wofstream translationFile(translationFileName);
+    std::wofstream updatedSourceFile(updatedSourceFileName);
+
+    //Turn the text buffers stream into a utf-8 encoded string.
+    openFileInUTF8(sourceFile, utf8_locale);
+    openFileInUTF8(translationFile, utf8_locale);
+    openFileInUTF8(updatedSourceFile, utf8_locale);
+    
+    formatTextFilesForTranslation(sourceFile, translationFile, updatedSourceFile);
 
 	return 0;
 }
